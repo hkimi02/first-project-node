34a228f6ea2c89beaca2ca8a8f18fb88768fd932
hkimi amin <hkimiamin02@gmail.com>
2023-10-06 00:35:16 +0100

    add login route and logic

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -2,6 +2,32 @@ import  express  from "express";
 
 import { createUser, getUser, getUserByEmail } from '../db/users'
 import { random,authentification } from "../helpers";
+
+export const login= async (req:express.Request,res:express.Response) => {
+        try{
+                const {email,password}=req.body;
+                if(!email || !password){
+                    return res.sendStatus(400).json({error:"missing fields"}).end();
+                }
+                const user=await getUserByEmail(email).select('+authentification.password +authentification.salt');
+                if(!user){
+                    return res.sendStatus(400).json({error:"user not found"}).end();
+                }
+                const expectedhash=authentification(user.authentification.salt,password);
+                if(user.authentification.password !== expectedhash){
+                    return res.sendStatus(403).json({error:"verify creditianls"}).end();
+                }
+                const salt=random();
+                user.authentification.sessionToken=authentification(salt,user._id.toString());
+
+                await user.save();
+                res.cookie('auth-token',user.authentification.sessionToken , {domain:'localhost',path:'/'});
+                return res.status(200).json(user).end();
+        }catch(error){
+            console.log(error);
+            return res.sendStatus(400);
+        }
+}
 export const register=async (req:express.Request,res:express.Response)=>{
     try{
         const {email,password,username}=req.body;
@@ -11,7 +37,6 @@ export const register=async (req:express.Request,res:express.Response)=>{
         const existingUser= await getUserByEmail(email);
         if(existingUser){
             return res.sendStatus(400);
-            return "hi2";
         }
 
         const salt=random();
