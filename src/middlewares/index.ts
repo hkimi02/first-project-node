8e4fa4c9d053c5e42d4a224fe9eaa612c38b386d
hkimi amin <hkimiamin02@gmail.com>
2023-10-06 23:39:35 +0100

    add isAuthenticated

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,1 +1,26 @@
-import express from 'express'
+import express, { NextFunction } from 'express';
+import {get,merge} from 'lodash';
+
+
+import { getUserBySessionToken } from '../db/users';
+
+export const isAuthenticated = async (req : express.Request, res : express.Response , next: express.NextFunction) => {
+    try{
+        const sessionToken=req.cookies['auth-token'];
+        if(!sessionToken){
+            return res.sendStatus(403);
+        }
+
+        const existingUser=await getUserBySessionToken(sessionToken);
+        if(!existingUser){
+            return res.sendStatus(403);
+        }
+
+        merge(req,{identity:existingUser});
+        return next;
+    }
+    catch(error){
+        console.log(error);
+        return res.sendStatus(400);
+    }
+}
